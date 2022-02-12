5596870748af25942cdea50dda2574690be7defe
YanMyoAung <pandora.2068@gmail.com>
2022-02-12 10:29:41 +0630

    practicing js basic

--- first.js
+++ first.js
@@ -1,1 +1,13 @@
-console.log('helloworld')
+let num = [1,2,3];
+let result = num.map(function(n){
+    return n + 1
+});
+
+let nums = [1,2,3,4];
+let filter = num.filter(function(n){
+    return n % 2;
+});
+
+let arr_1 = [2,3,4];
+let arr_2 = [3,4,5];
+console.log(...arr_1,...arr_2);
